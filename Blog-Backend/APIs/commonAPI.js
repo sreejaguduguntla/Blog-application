8826694e40a656f0449bec18565236daf92a5c78
Sreeja Guduguntla <sreejaguduguntla@gmail.com>
2026-05-18 13:47:19 +0530

    fixed production auth cookies

--- Blog-Backend/APIs/commonAPI.js
+++ Blog-Backend/APIs/commonAPI.js
@@ -13,9 +13,9 @@ commonRouter.post("/login", async (req, res) => {
   let { token, user } = await authenticate(userCred);
   //save tokan as httpOnly cookie
   res.cookie("token", token, {
-    httpOnly: true,
-    sameSite: "lax",
-    secure: false,
+    httpOnly: true, // Must match original  settings
+    secure: true, // Must match original  settings
+    sameSite: "none", // Must match original  settings
   });
   //send res
   res.status(200).json({ message: "login success", payload: user });
@@ -26,8 +26,8 @@ commonRouter.get("/logout", (req, res) => {
   // Clear the cookie named 'token'
   res.clearCookie("token", {
     httpOnly: true, // Must match original  settings
-    secure: false, // Must match original  settings
-    sameSite: "lax", // Must match original  settings
+    secure: true, // Must match original  settings
+    sameSite: "none", // Must match original  settings
   });
 
   res.status(200).json({ message: "Logged out successfully" });
